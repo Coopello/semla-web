b709c92146bb7d908a52f981d41c1a52bbf3dbb7
github akito-10 <akt.fukuda@gmail.com>
2022-08-15 03:30:30 +0900

    💄 サイドメニュー

---
+++ src/pageComponent/index/SideMenuArea.tsx
@@ -0,0 +1,11 @@
+/**
+ * @package
+ */
+export const SideMenuArea = () => (
+  <ul className="flex h-fit list-inside list-disc flex-col gap-4 rounded-xl bg-white p-8 text-lg font-bold">
+    <li>一言まとめ</li>
+    <li>調べたことを深掘り</li>
+    <li>自分の考え</li>
+    <li>参考文献</li>
+  </ul>
+);

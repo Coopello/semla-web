7e97acc4f10cb4266f263d3433906ad580a652f4
github akito-10 <akt.fukuda@gmail.com>
2022-08-15 03:32:56 +0900

    🔨 各項目の定数ファイル作成

---
+++ src/pageLib/TITLE_ITEMS.ts
@@ -0,0 +1,23 @@
+import type { TitleItem } from "src/type";
+
+/**
+ * @package
+ */
+export const TITLE_ITEMS: TitleItem[] = [
+  {
+    description: "調べたことに関して、一言でまとめたものをここに書きましょう！",
+    title: "一言まとめ",
+  },
+  {
+    description: "調べたことに関して、深掘りした内容をここに書きましょう！",
+    title: "調べたことを深掘り",
+  },
+  {
+    description: "調べたことに関して、自分の考えをここに書きましょう！",
+    title: "自分の考え",
+  },
+  {
+    description: "調べるにあたって参考にしたサイトなどをここに書きましょう！",
+    title: "参考文献",
+  },
+];
---
+++ src/type/index.ts
@@ -0,0 +1,1 @@
+export type { TitleItem } from "./TItleItem";

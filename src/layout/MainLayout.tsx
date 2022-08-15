87bf8c951805c8dfbc2e7fb71688614fcb937bbd
github akito-10 <akt.fukuda@gmail.com>
2022-08-15 11:11:43 +0900

    📱 レスポンシブ対応

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,8 +1,17 @@
 import type { CustomLayout } from "next";
+import { useScreenSize } from "src/lib";
 
 /**
  * @package
  */
-export const MainLayout: CustomLayout = (page) => (
-  <main className="min-h-screen bg-custom-gray p-16">{page}</main>
-);
+export const MainLayout: CustomLayout = (page) => {
+  const { isMobile } = useScreenSize();
+
+  return (
+    <main
+      className={`min-h-screen bg-custom-gray  ${isMobile ? "p-4" : "p-16"}`}
+    >
+      {page}
+    </main>
+  );
+};

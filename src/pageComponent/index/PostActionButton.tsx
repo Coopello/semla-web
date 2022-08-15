eb60e536295778aa9ff0eca14b3fc9ce3312484e
github akito-10 <akt.fukuda@gmail.com>
2022-08-15 11:11:21 +0900

    💄 モバイル用送信ボタン

---
+++ src/pageComponent/index/PostActionButton.tsx
@@ -0,0 +1,10 @@
+import { PostIcon } from "public/svg";
+
+/**
+ * @package
+ */
+export const PostActionButton = () => (
+  <span className="fixed bottom-6 right-4 cursor-pointer">
+    <PostIcon />
+  </span>
+);

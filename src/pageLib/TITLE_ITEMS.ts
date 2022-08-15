ef5cda10ead1f4e2e554612af5ee67e09d75053c
github akito-10 <akt.fukuda@gmail.com>
2022-08-15 11:10:38 +0900

    💬

--- src/pageLib/TITLE_ITEMS.ts
+++ src/pageLib/TITLE_ITEMS.ts
@@ -5,19 +5,19 @@ import type { TitleItem } from "src/type";
  */
 export const TITLE_ITEMS: TitleItem[] = [
   {
-    description: "調べたことに関して、一言でまとめたものをここに書きましょう！",
+    description: "一言で背景をまとめましょう！",
     title: "一言まとめ",
   },
   {
-    description: "調べたことに関して、深掘りした内容をここに書きましょう！",
+    description: "調べたこと・勉強したことを、まとめてみましょう！",
     title: "調べたことを深掘り",
   },
   {
-    description: "調べたことに関して、自分の考えをここに書きましょう！",
+    description: "自分の考えを結論としてまとめてみましょう！",
     title: "自分の考え",
   },
   {
-    description: "調べるにあたって参考にしたサイトなどをここに書きましょう！",
+    description: "参考にした文献をまとめましょう！",
     title: "参考文献",
   },
 ];

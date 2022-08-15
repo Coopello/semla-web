390222f7c787753f2b4e53258d96276f3a3d7006
github akito-10 <akt.fukuda@gmail.com>
2022-08-15 11:11:01 +0900

    💄 モバイル用のヘッダー

---
+++ src/pageComponent/index/HeaderArea.tsx
@@ -0,0 +1,10 @@
+import { SemlaIcon } from "public/svg";
+
+/**
+ * @package
+ */
+export const HeaderArea = () => (
+  <div className="absolute top-0 left-0 flex h-[72px] w-full items-center bg-white pl-2">
+    <SemlaIcon />
+  </div>
+);

0370e9f4c4d5304f20c6c4c7d06804bd7ce9d63d
github akito-10 <akt.fukuda@gmail.com>
2022-08-23 11:19:00 +0900

    💄 BottomActionの上ボーダーの色を修正

--- src/pageComponent/index/BottomActionArea.tsx
+++ src/pageComponent/index/BottomActionArea.tsx
@@ -6,7 +6,7 @@ import { Button } from "src/component/Button";
  * @package
  */
 export const BottomActionArea: FC<{ onClick: () => void }> = ({ onClick }) => (
-  <div className="fixed bottom-0 left-0 flex w-full items-center justify-between border-t border-gray-400 bg-white py-4 px-8">
+  <div className="fixed bottom-0 left-0 flex w-full items-center justify-between border-t border-gray-200 bg-white py-4 px-8">
     <SemlaIcon />
     <Button onClick={onClick}>Post!</Button>
   </div>

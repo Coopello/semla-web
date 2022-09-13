f17bfd4c7b22bf599de63c96c0a79bef7398bb14
akito <63759669+akito-10@users.noreply.github.com>
2022-09-13 21:33:33 +0900

    ビルドエラーの修正 (#10)

--- src/pageComponent/index/BottomActionArea.tsx
+++ src/pageComponent/index/BottomActionArea.tsx
@@ -1,4 +1,4 @@
-import { SemlaIcon } from "public/svg";
+import { CoopemoIcon } from "public/svg";
 import type { FC } from "react";
 import { Button } from "src/component/Button";
 
@@ -7,7 +7,7 @@ import { Button } from "src/component/Button";
  */
 export const BottomActionArea: FC<{ onClick: () => void }> = ({ onClick }) => (
   <div className="fixed bottom-0 left-0 flex w-full items-center justify-between border-t border-gray-200 bg-white py-4 px-8">
-    <SemlaIcon />
+    <CoopemoIcon />
     <Button onClick={onClick}>Post!</Button>
   </div>
 );
